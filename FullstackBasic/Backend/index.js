7468687e1f09bffcc915bcbad36023f4f6b79c63
Tanmay Sharma <tanmaysharmats36@gmail.com>
2025-02-12 19:44:07 +0530

    Proxy

--- FullstackBasic/Backend/index.js
+++ FullstackBasic/Backend/index.js
@@ -1,43 +1,48 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
+
 app.get('/', (req, res) => {
-  res.send('Hello World');
+  res.send('Hello World!');
 });
 
-//get a list of 5 jokes
-
-app.get('/jokes', (req, res) => {
-  const jokes = [
+app.get('/api/jokes', (req, res) => {
+  const jokes = [   
     {
       id: 1,
-      title : 'Why did the scarecrow win an award?',
-      joke: 'Why did the scarecrow win an award? Because he was outstanding in his field!',
+      question: 'Why did the scarecrow win an award?',
+      joke: 'Why did the scarecrow win an award? Because he was outstanding in his field!'
     },
     {
       id: 2,
-      title : 'What do you call a fish wearing a crown?',
-      joke: 'What do you call a fish wearing a crown? A king fish!',
+      question: 'What do you call a fish wearing a crown?',
+      joke: 'What do you call a fish wearing a crown? King Cod!'
     },
     {
       id: 3,
-      title : 'What do you call a pile of cats?',
-      joke: 'What do you call a pile of cats? A meowtain!',
+      question: 'What do you call a pile of cats?',
+      joke: 'What do you call a pile of cats? A meowtain!'
     },
     {
       id: 4,
-      title : 'What do you call a bear with no teeth?',
-      joke: 'What do you call a bear with no teeth? A gummy bear!',
+      question: 'Why did the tomato turn red?',
+      joke: 'Why did the tomato turn red? Because it saw the salad dressing!'
     },
     {
       id: 5,
-      title : 'What do you call a pile of cats?',
-      joke: 'What do you call a pile of cats? A meowtain!',
+      question: 'What do you call a bear with no teeth?',
+      joke: 'What do you call a bear with no teeth? A gummy bear!'
     },
-  ];
+    {
+      id: 6,
+      question: 'What do you call a pile of cats?',
+      joke: 'What do you call a pile of cats? A meowtain!'
+    },
+
+  ];  
   res.send(jokes);
 });
 
 app.listen(port, () => {
-  console.log('Server is running on port 3000');
+  console.log(`Server listening on port ${port}`);
 });
